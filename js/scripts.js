0d9e51b858e44ace11fd1f952660b7e47aa3ead5
Noah Ramey <noahramey1997@gmail.com>
2016-06-17 09:42:03 -0700

    add price by size spec

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+// BUSINESS LOGIC //
+function Pizza(name, size){
+  this.name = name;
+  this.size = size;
+  this.toppings = [];
+  this.price = 0;
+}
+
+Pizza.prototype.getPriceForSize = function() {
+  if (this.size === "Small") {
+    this.price += 8;
+  } else if (this.size === "Medium") {
+    this.price += 9.75;
+  } else {
+    this.price += 11.50;
+  }
+}
+
+Pizza.prototype.getPriceForToppings = function() {
+  if (this)
+}
+
+
+
+
+
+
+// USER INTERFACE LOGIC //
+$(function() {
+  $("#add-topping").click(function() {
+    $(".all-toppings").append('<div class="form-group">' +
+                                '<div class="new-topping">' +
+                                  '<label for="new-toppings">What other toppings would you like?</label>' +
+                                    '<select class="form-control" id="new-toppings">' +
+                                      '<option>Pepperoni</option>' +
+                                      '<option>Mushrooms</option>' +
+                                      '<option>Onions</option>' +
+                                      '<option>Sausage</option>' +
+                                      '<option>Bacon</option>' +
+                                      '<option>Extra Cheese</option>' +
+                                      '<option>Black Olives</option>' +
+                                      '<option>Green Peppers</option>' +
+                                      '<option>Pineapple</option>' +
+                                      '<option>Ham</option>' +
+                                    '</select>' +
+                                  '</div>' +
+                                '</div>');
+  });
+
+  $("#pizza-options").submit(function(event) {
+    event.preventDefault();
+
+    var nameInput = $("#name").val();
+    var sizeInput = $("#size").val();
+    var newPizza = new Pizza(nameInput, sizeInput);
+
+    newPizza.getPriceForSize();
+
+    $(".new-topping").each(function() {
+      var toppingInput = $(this).find("#new-toppings").val();
+      newPizza.toppings.push(toppingInput);
+    });
+
+    $("#pizza-options").hide();
+    $(".total-price").fadeIn();
+
+    console.log(newPizza);
+  });
+});
